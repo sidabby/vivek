24d69dd093115a7dee167b1e4fee0eb467856c07
sidabby <45937752+sidabby@users.noreply.github.com>
2018-12-17 16:35:07 +0530

    Create index.php

---
+++ index.php
@@ -0,0 +1,12 @@
+<html>
+<head>
+<title>TEST|HOME</title>
+</head>
+<body>
+<p>
+hello there
+      this is the first project on git.
+      hope you are enjoying the development 
+</p>
+</body>
+</html>
